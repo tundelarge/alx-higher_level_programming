e3bd4895ce3c025fcbbc3fe53f5a5dde846c6a72
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2022-12-05 20:51:02 +0100

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,8 @@
-qwertyu
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for element in matrix:
+        for number in element:
+            print("{:d}".format(number), end="")
+            if(number is not element[len(element)-1]):
+                print(" ", end="")
+        print()
