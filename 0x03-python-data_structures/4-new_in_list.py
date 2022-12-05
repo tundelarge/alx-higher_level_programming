9c3b643e2d3a96359b8ef0033daa01ff8d0f1bd4
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2022-12-05 20:47:47 +0100

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,7 @@
-qwerty
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if(idx < 0 or idx >= len(my_list)):
+        return(my_list)
+    a = my_list[:]
+    a[idx] = element
+    return(a)
