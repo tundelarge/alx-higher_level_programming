659eb57f89218f190ff9a10dd3b7caef804c5bf4
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-01-04 19:41:48 +0100

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,24 +1,16 @@
+#!/usr/bin/python3
+"""Module containing a function to print first and last name"""
 
->>> say_my_name = __import__("3-say_my_name").say_my_name
->>> say_my_name("abebe", "beso")
-My name is abebe beso
->>> say_my_name("chala")
-My name is chala 
->>> say_my_name(5)
-Traceback (most recent call last):
-    ...
-TypeError: first_name must be a string
->>> say_my_name("chala", 30)
-Traceback (most recent call last):
-    ...
-TypeError: last_name must be a string
->>> say_my_name(50, 30)
-Traceback (most recent call last):
-    ...
-TypeError: first_name must be a string
->>> say_my_name("", "jo")
-My name is  jo
->>> say_my_name()
-Traceback (most recent call last):
-  ...
-TypeError: say_my_name() missing 1 required positional argument: 'first_name'
+
+def say_my_name(first_name, last_name=""):
+    """ prints first and last name
+        Arguments:
+            @first_name: first name to be printed
+            @second_name: last_name to be printed
+    """
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
