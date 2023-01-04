64787319e17c78b115fcd5bbaf69ec13ca633a45
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-01-04 15:27:20 +0100

    Update 4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
+"""Module containing a function that prints a square"""
+
+
 def print_square(size):
-    if not isinstance(size, int):
+    """ adds integers
+        Arguments:
+            @size: size of the square
+    """
+
+    if type(size) is not int:
         raise TypeError("size must be an integer")
-    if size < 0:
+    if (size < 0):
         raise ValueError("size must be >= 0")
-    for index in range(size):
-        print("x" * size)
+    if size == 0:
+        return
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
