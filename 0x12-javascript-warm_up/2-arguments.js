2140f9e4e3aff9cb7a61443c77dde3db57a4d453
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-03-13 12:46:29 +0100

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
-const { argv } = require('process');
-if (argv[2] === undefined) { console.log('No argument'); } else { console.log(argv[2]); }
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
