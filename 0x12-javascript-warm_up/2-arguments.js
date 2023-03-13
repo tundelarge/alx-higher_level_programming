eec0065c1e78d6f1d9450cc2f21ae2963c9178a6
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-03-13 12:41:38 +0100

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
-/*
-   Commandline arguments in js is made possible using the process.argv
-   which is an array that contains all arguments of the CLI.
-   process.argv[0] contains the absolute path to the node binary file
-   process.argv[1] contains the absolute file path to the .js file in
-   which the js engine runs.
-   so process.argv[2] ... contains users passed in arguments.
-*/
+const { argv } = require('process');
+if (argv[2] === undefined) { console.log('No argument'); } else { console.log(argv[2]); }
