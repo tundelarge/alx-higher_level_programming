8fd9073a886b5e77ede4fdb89a9ce31c720ebce8
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-03-14 19:07:28 +0100

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
-const dKeys = Object.keys(dict);
-const values = Object.values(dict);
-let matched;
-const result = {};
-// loop over the values
-for (let i = 0; i < values.length; i++) {
-  result[JSON.stringify(values[i])] = [];
-  matched = dKeys.filter(key => dict[key] === values[i]);
-  matched.forEach(item => result[JSON.stringify(values[i])].push(item));
+const newDict = {};
+for (const key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
+  }
 }
+console.log(newDict);
