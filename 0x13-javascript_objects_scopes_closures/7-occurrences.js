4d79f0042253ac912cc45fad00439454c027b825
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-03-14 13:52:47 +0100

    Update 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,10 @@
-qwertyu
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let nOccurrences = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
+      nOccurrences++;
+    }
+  }
+  return nOccurrences;
+};
