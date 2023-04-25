6e97d752be63d86a0921c3e31749e4c761c04ba5
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-04-25 18:42:35 +0100

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,6 @@
-qwertyu
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
+});
