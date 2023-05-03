08d17c9d7b9b8209aa73af2cfda9baa697e0aee2
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-05-03 20:35:59 +0100

    Update 102-script.js

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,1 +1,11 @@
-qwertyu
+$(document).ready(function () {
+	$("INPUT#btn_translate").click(function () {
+		const language_code = $("INPUT#language_code").val();
+		$.getJSON(
+			`https://fourtonfish.com/hellosalut/hello/?lang=${language_code}`,
+			function (data) {
+				$("#hello").text(data.hello);
+			}
+		);
+	});
+});
